4abe84c1b9c71976f238a59c2c65f25981dbf380
Illia <illia.vasiliev@gmail.com>
2023-06-13 15:53:42 +0300

    init hw4

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+const { handleMongooseError } = require('../middlewares');
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, 'Set password for user'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ['starter', 'pro', 'business'],
+      default: 'starter',
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post('save', handleMongooseError);
+
+const Users = model('contact', userSchema);
+
+module.exports = Users;
